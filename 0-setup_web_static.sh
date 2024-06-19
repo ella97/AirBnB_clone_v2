b4025f97264ac85df15aa9470d0d698484111ec0
ella97 <nuwymwanri@gmail.com>
2024-06-19 13:59:22 +0300

    Changelog: New Corrections

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,13 @@
-#!/usr/bin/env bash
-# script that sets up web servers for the deployment of web_static
+#!/bin/bash
+# Update package lists and install Nginx
 sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+sudo apt-get install -y nginx
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
+# Set up the directory structure
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+
+# Create a fake HTML file to test the Nginx configuration
+echo "<html>
   <head>
   </head>
   <body>
@@ -18,10 +15,34 @@ sudo echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+# Create a symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
+# Set ownership of the /data/ folder to the ubuntu user AND group
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+# Update the Nginx configuration to serve the content of /data/web_static/current/
+# Replace the default configuration with the following lines
+sudo tee /etc/nginx/sites-available/default > /dev/null << EOL
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
 
-sudo service nginx restart
+    root /var/www/html;
+    index index.html index.htm;
+
+    server_name _;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+}
+EOL
+
+# Restart Nginx to apply the changes
+sudo systemctl restart nginx
