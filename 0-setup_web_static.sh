c47f5160c1275e8d3d285483d5344b6a111887be
ella97 <nuwymwanri@gmail.com>
2024-06-19 14:24:35 +0300

    Changelog: New Corrections

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,50 +1,38 @@
 #!/usr/bin/env bash
-# Update package lists and install Nginx
-sudo apt-get update
-sudo apt-get install -y nginx
+# Sets up a web server for deployment of web_static.
 
-# Set up the directory structure
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-# sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+apt-get update
+apt-get install -y nginx
 
-# Create a fake HTML file to test the Nginx configuration
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Create a symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-# Set ownership of the /data/ folder to the ubuntu user AND group
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update the Nginx configuration to serve the content of /data/web_static/current/
-# Replace the default configuration with the following lines
-sudo tee /etc/nginx/sites-available/default > /dev/null << EOL
-server {
+printf %s "server {
     listen 80 default_server;
     listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html index.htm;
-
-    server_name _;
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
     }
-}
-EOL
 
-# Restart Nginx to apply the changes
-sudo systemctl restart nginx
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
